6c742688e5f70ec42a795781d41e2900c9bf25ce
fire774 <65456795+natealberti@users.noreply.github.com>
2022-11-11 23:09:54 -0700

    adding text to progress screen

--- progressGUI.java
+++ progressGUI.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
@@ -16,11 +17,13 @@ public class progressGUI extends JPanel{
    // ProgressBar progressBar = new ProgressBar(0);
     JProgressBar jb;    
     JProgressBar progressBAR;
+	JLabel timeReady, emailReceipt;
+	JPanel panel;
     int i=0,num=0;     
     private JButton ReturnHome;
   
     public  progressGUI(JFrame frame, Order o) {
-  
+		frame.getContentPane();
     
 
     	// button
@@ -29,18 +32,34 @@ public class progressGUI extends JPanel{
 
     	
     	   
-        
+        // progress bar
     	progressBAR=new JProgressBar(0,5);    
     	progressBAR.setBounds(40,40,160,30);         
     	progressBAR.setValue(0);    
-    	progressBAR.setStringPainted(true);    
-        
+    	progressBAR.setStringPainted(true);
 
-  
+		// thank you text
+		timeReady = new JLabel("Your order will be ready to pick up at " + o.getPickUpTime());
+    	timeReady.setFont(new Font("Arial", Font.PLAIN, 20));
+    	timeReady.setLocation(300, 700);
+
+		emailReceipt = new JLabel("A receipt has been sent to " + o.getCustomerEmail());
+    	emailReceipt.setFont(new Font("Arial", Font.PLAIN, 20));
+    	emailReceipt.setBounds(300, 600, 100, 75);
+
+		// SET LOCATION OF JLABEL
+		panel = new JPanel();
+		panel.setSize(600, 300);
+		panel.setLocation(0, 300);
+		panel.add(timeReady);
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		frame.add(panel);
 
     	// add the elements
         add(ReturnHome);
         add(progressBAR);
+		//add(timeReady);
+		add(emailReceipt);
      
       
   
