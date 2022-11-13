6278831ad28797dbf796cb98c2acb3fd3197163d
conrarclutch <114435603+conrarclutch@users.noreply.github.com>
2022-11-12 21:11:20 -0800

    Update chefsViewGui.java

--- chefsViewGui.java
+++ chefsViewGui.java
@@ -6,20 +6,79 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class chefsViewGui extends JPanel {
-	 private JPanel orderNum, name ,pizza_panel ;
-	 private JLabel orderNUM , customer , pizzaType;
-	public chefsViewGui(JFrame frame, Order i) {
+public class chefsViewGui extends JPanel  {
+	 private JPanel orderNum, name ,pizza_panel , orderInfo ;
+	 private JLabel orderNUM , customer , pizzaType,jLabel , pizzaTop;
+	public chefsViewGui(JFrame frame, Order i , progressGUI p) {
 		
-		setLayout(new FlowLayout(FlowLayout.CENTER, 40, 40));
-		orderNum = new JPanel();
-		orderNum.setLayout(new GridLayout(10,10,10,10));
+		FlowLayout layout = new FlowLayout();
+		layout.setAlignment(FlowLayout.LEFT);
 		
-		//orderNUM = new JLabel(i.getOrderNum());
+		String[] orderProgressOptions = {"ORDER RECIEVED" ,"IN PREP", "COOKING", "READY FOR PICK UP"};
+
 		
 		
-		add(orderNum);
+		customer = new JLabel("Customer Name: " + i.getCustomerName());
+		customer.setFont(new Font("Arial", Font.PLAIN, 30));
+		customer.setLocation(1080/3, 100);
+		
+		orderNUM = new JLabel("Order Number: " +Integer.toString(i.getOrderNum()));
+		orderNUM.setFont(new Font("Arial", Font.PLAIN, 30));
+		orderNUM.setLocation(1080/2, 100);
+		
+		pizzaType= new JLabel("Pizza Type: "+i.getPizzaType());
+		pizzaType.setFont(new Font("Arial", Font.PLAIN, 20));
+		pizzaType.setLocation(1080/2, 100);
+		
+		pizzaTop =new JLabel("Toppings: " + i.getPizzaTopping()+ " ");
+		pizzaTop.setFont(new Font("Arial", Font.PLAIN, 20));
+		pizzaTop.setLocation(1080/2, 100);
+		
+		
+		
+		
+		final JComboBox<String> status = new JComboBox<String>(orderProgressOptions);
+		status.setVisible(true);
+		
+		status.getItemAt(status.getSelectedIndex());
+		JButton update = new JButton("Update Order Status");
+		
+		
+		
+		
+		Box orderInfo = Box.createVerticalBox();
+		orderInfo.add(customer);
+		orderInfo.add(orderNUM);
+		orderInfo.add(pizzaType);
+		orderInfo.add(pizzaTop);
+		
+		orderInfo.add(status);
+		orderInfo.add(update);
+		
+	
+		add(orderInfo);
+	
+		
+		update.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String newOrderProgress = status.getItemAt(status.getSelectedIndex());
+                System.out.println(newOrderProgress);
+                
+                updateOrderProgress(status.getSelectedIndex());
+                p.updateProgress(status.getSelectedIndex());
+             
+			}
+		});
 		
 	}
+	
+	public void updateOrderProgress(int i) {
+		System.out.println(i);
+		
+		
+	}
+
+
 
 }
